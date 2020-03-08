98bca48c48f1f0bb6d9aec0cb32ade8ea01a723f
Mirac Ozkan <mirac.ozkan123@gmail.com>
2020-03-08 22:37:08 +0300

    toolbar title is added

--- app/src/main/java/com/miracozkan/hipoandroidintern/ui/main/MainActivity.kt
+++ app/src/main/java/com/miracozkan/hipoandroidintern/ui/main/MainActivity.kt
@@ -2,6 +2,7 @@ package com.miracozkan.hipoandroidintern.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.miracozkan.hipoandroidintern.R
 import com.miracozkan.hipoandroidintern.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        supportActionBar?.title = getString(R.string.members)
         setContentView(binding.root)
     }
 }
